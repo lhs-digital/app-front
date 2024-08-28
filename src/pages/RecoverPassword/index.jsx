391b28f2a6bd065606d3f519e84b0c22e062df24
Jvictor-7 <jvcl@ic.ufal.br>
2024-08-28 16:12:47 -0300

    feat: Integração e telas de recuperar senha concluidas.

--- src/pages/RecoverPassword/index.jsx
+++ src/pages/RecoverPassword/index.jsx
@@ -1,10 +1,40 @@
 import React, { useState } from 'react'
 import './signin.css'
 import logo from '../../assets/logo.jpg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import api from '../../services/api'
 
 const RecoverPassword = () => {
   const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  
+    if (!email) {
+      toast.warning('Preencha o email para que o código seja enviado!')
+      return
+    }
+
+    try {
+      const response = await api.post(`/password-email`, {
+        email,
+      });
+
+      if (response.data.status !== "error") {
+        toast.success('Código de recuperação enviado com sucesso!')
+        setTimeout(() => {
+          navigate('/password-token/6qTbhh3UyjGc6xbjEJedPOCSA8d3l0fiSLIqLHB2SQjO8Zt37m6LSPjl1syw')
+        }, 3000)
+      } else {
+        toast.error('Email não cadastrado no sistema!')
+      }
+    } catch (error) {
+      console.error("Recuperação de senha falhou:", error);
+      throw error;
+    }
+  }
 
   return (
     <div className='container-center'>
@@ -15,13 +45,13 @@ const RecoverPassword = () => {
           <p>Sistema de Gerenciamento de Provedores</p>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <p>Um código de recuperação de senha será enviado ao seu email, caso esteja cadastrado no sistema.</p>
           <input
             type="email"
             placeholder="Insira o seu email"
             value={email}
-            onChange={ (e) => setEmail(e.target.value) }
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <button type='submit'>Enviar Código</button>
