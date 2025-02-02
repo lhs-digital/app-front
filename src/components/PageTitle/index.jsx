f094652322448e216d7531f78549e8a8729844da
Isadora Paz <mibp@ic.ufal.br>
2025-02-02 12:36:02 -0300

    feat: Titulos e favicon

--- src/components/PageTitle/index.jsx
+++ src/components/PageTitle/index.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
+
 const Index = ({ title, subtitle = "", buttons = <></> }) => {
+  useEffect(() => {
+    document.title = `LHS - ${title}`;
+  }, [title]);
+
   return (
     <div className="flex flex-row items-center justify-between w-full">
       <div>
-        <div className="flex flex-row items-center gap-4 text-xl font-bold">
-          {title}
-        </div>
+        <p className="text-xl font-bold">{title}</p>
         <div className="text-gray-400">{subtitle}</div>
       </div>
       <div className="flex flex-row items-center gap-4">{buttons}</div>
