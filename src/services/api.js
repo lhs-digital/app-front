f364062babab8b2445c644196ca84102ac3c7e87
Isadora Paz <mibp@ic.ufal.br>
2025-03-08 13:40:11 -0300

    refactor: Use new auth key from localstorage

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(async (config) => {
-  const JWT = localStorage.getItem("token") || sessionStorage.getItem("token");
+  const JWT = localStorage.getItem("_auth") || sessionStorage.getItem("_auth");
   if (JWT != null) {
     config.headers.Authorization = `Bearer ${JWT}`;
   }
