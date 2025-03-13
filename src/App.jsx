339b43ca16f4a68ff489f7f6789bd53967530124
Isadora Paz <mibp@ic.ufal.br>
2025-03-13 12:55:38 -0300

    config: Add tanstack query provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "react-auth-kit";
 import createStore from "react-auth-kit/createStore";
 import { RouterProvider } from "react-router-dom";
@@ -13,16 +14,28 @@ const store = createStore({
   authName: "_auth",
 });
 
+const qc = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      retry: true,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const App = () => {
   return (
     <ThemeProvider theme={baseTheme}>
       <CssBaseline />
-      <AuthProvider store={store}>
-        <UserStateProvider>
-          <RouterProvider router={AppRoutes} />
-          <ToastContainer />
-        </UserStateProvider>
-      </AuthProvider>
+      <QueryClientProvider client={qc}>
+        <AuthProvider store={store}>
+          <UserStateProvider>
+            <RouterProvider router={AppRoutes} />
+            <ToastContainer position="bottom-right" />
+          </UserStateProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
