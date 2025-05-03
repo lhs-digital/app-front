08a025b135ebc0dfaf11ce30b33434978a7d2c22
Isadora Paz <mibp@ic.ufal.br>
2025-05-03 15:59:25 -0300

    fix: company context before login

--- src/contexts/companyContext.jsx
+++ src/contexts/companyContext.jsx
@@ -1,18 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useUserState } from "../hooks/useUserState";
 import api from "../services/api";
 
 export const CompanyContext = createContext();
 
 export const CompanyContextProvider = ({ children }) => {
-  const { isLighthouse, company: userCompany } = useUserState().state;
+  const { state: user } = useUserState();
 
   const [company, setCompany] = useState(
-    isLighthouse
+    user?.isLighthouse
       ? JSON.parse(localStorage.getItem("company")) || null
-      : userCompany,
+      : user?.company || null,
   );
+
+  useEffect(() => {
+    if (user?.isLighthouse) {
+      const storedCompany = JSON.parse(localStorage.getItem("company"));
+      if (storedCompany) setCompany(storedCompany);
+    } else {
+      setCompany(user?.company);
+    }
+  }, [user]);
 
   const changeCompany = (company) => {
     console.log("Company changed to: ", company);
@@ -27,10 +36,14 @@ export const CompanyContextProvider = ({ children }) => {
         const response = await api.get("/companies");
         return response.data.data.filter((company) => company.id !== 1);
       },
-      enabled: isLighthouse,
+      enabled: user?.isLighthouse,
       refetchInterval: 60000,
       refetchOnWindowFocus: false,
     });
+
+  if (!user) {
+    return children;
+  }
 
   return (
     <CompanyContext.Provider
