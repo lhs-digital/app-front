77e56be5ca753fcee95274ce541a9049629c9227
Isadora Paz <mibp@ic.ufal.br>
2025-05-25 12:03:50 -0300

    feat: <WIP> AuditTables

--- src/modules/audit/AuditTables/index.jsx
+++ src/modules/audit/AuditTables/index.jsx
@@ -1,5 +1,55 @@
+import { TableChart } from "@mui/icons-material";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useCompany } from "../../../hooks/useCompany";
+import PageTitle from "../../../layout/components/PageTitle";
+import api from "../../../services/api";
+
 const AuditTables = () => {
-  return <div>AuditTables</div>;
+  const { company } = useCompany();
+  const [table, setTable] = useState("");
+
+  const { data: tables = [], isLoading: areTablesLoading } = useQuery({
+    queryKey: ["company_tables", company],
+    queryFn: async () => {
+      const response = await api.get(`/company/auditable_tables`, {
+        params: { company_id: company.id },
+      });
+      setTable(response.data.data[0]);
+      return response.data.data;
+    },
+    enabled: !!company,
+  });
+  return (
+    <div className="flex flex-col gap-6 w-full">
+      <PageTitle
+        title="Tabelas de auditoria"
+        icon={<TableChart />}
+        subtitle="Visualize entidades das tabelas auditáveis"
+      />
+      <div className="w-full lg:w-1/3">
+        <FormControl fullWidth>
+          <InputLabel id="table-select">Tabela</InputLabel>
+          <Select
+            fullWidth
+            labelId="table-select"
+            className="capitalize"
+            value={table}
+            label="Tabela"
+            disabled={areTablesLoading}
+            onChange={(e) => setTable(e.target.value)}
+          >
+            {tables.map((table) => (
+              <MenuItem key={table.id} value={table} className="capitalize">
+                {table.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
 };
 
 export default AuditTables;
