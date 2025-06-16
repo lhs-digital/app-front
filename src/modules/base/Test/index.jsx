859a1544792763cf466a1df0c39162e86b0a30c2
Isadora Paz <mibp@ic.ufal.br>
2025-06-16 11:17:10 -0300

    reset dev page

--- src/modules/base/Test/index.jsx
+++ src/modules/base/Test/index.jsx
@@ -1,24 +1,13 @@
-import { BugReport, Save } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Web } from "@mui/icons-material";
 import PageTitle from "../../../layout/components/PageTitle";
 
 const TestPage = () => {
   return (
-    <div className="flex flex-col gap-4 w-full">
+    <div className="flex flex-col gap-6 w-full">
       <PageTitle
-        title="Test Page"
-        icon={<BugReport />}
-        buttons={[
-          <Button
-            key="submit-form"
-            type="submit"
-            variant="contained"
-            startIcon={<Save />}
-          >
-            SALVAR
-          </Button>,
-        ]}
-        subtitle="Page for developing components and testing features"
+        title="Desenvolvimento"
+        icon={<Web />}
+        subtitle="Esta página é destinada para testes e desenvolvimento"
       />
     </div>
   );
