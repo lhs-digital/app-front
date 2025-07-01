b8e215bc8b30df45d8bdb069f34603bc09decfcd
Isadora Paz <mibp@ic.ufal.br>
2025-07-01 16:04:12 -0300

    feat: tag for page title

--- src/layout/components/PageTitle.jsx
+++ src/layout/components/PageTitle.jsx
@@ -1,9 +1,11 @@
 import { TableChartOutlined } from "@mui/icons-material";
+import { Chip } from "@mui/material";
 import { useEffect } from "react";
 
 const PageTitle = ({
   title,
   subtitle = "",
+  tag = "",
   buttons = <></>,
   icon = <TableChartOutlined fontSize="small" />,
 }) => {
@@ -18,7 +20,16 @@ const PageTitle = ({
           <span className="mb-0.5">{icon}</span>
           <p className="text-xl font-bold">{title}</p>
         </div>
-        <div className="text-sm text-gray-400">{subtitle}</div>
+        <div className="text-sm text-neutral-400 flex flex-row gap-2 items-center">
+          {subtitle}
+          {tag && (
+            <Chip
+              label={<p className="text-xs text-neutral-500">{tag}</p>}
+              size="small"
+              variant="outlined"
+            />
+          )}
+        </div>
       </div>
       <div className="flex flex-row items-center gap-4">{buttons}</div>
     </div>
