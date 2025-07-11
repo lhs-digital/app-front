6cb8b532296a55c8646db8ad941a6dd3e5f0f928
Isadora Paz <mibp@ic.ufal.br>
2025-07-11 16:39:29 -0300

    feat: better rule creation

--- src/components/AuditComponents/Validation.jsx
+++ src/components/AuditComponents/Validation.jsx
@@ -1,7 +1,16 @@
-import { Chip } from "@mui/material";
-import { validationLabels } from "../../services/utils";
+import { Chip, Tooltip } from "@mui/material";
+import { useThemeMode } from "../../contexts/themeModeContext";
+import {
+  formattedPriority,
+  getPriorityColor,
+  validationLabels,
+} from "../../services/utils";
+import { handleMode } from "../../theme";
 
 const Validation = ({ rule, params, onDelete }) => {
+  const { mode: themeMode } = useThemeMode();
+  const theme = handleMode(themeMode);
+  const colors = getPriorityColor(rule.priority, theme);
   const formatParams = () => {
     if (!params) return null;
     if (Array.isArray(params)) {
@@ -26,7 +35,41 @@ const Validation = ({ rule, params, onDelete }) => {
     return validationLabels[rule.name];
   };
 
-  return <Chip label={handleLabel()} onDelete={onDelete} />;
+  return (
+    <Tooltip
+      placement="top"
+      arrow
+      title={
+        <div className="flex flex-col gap-1">
+          <p>
+            Mensagem de erro:{" "}
+            <span className="font-bold">
+              {rule.message || "(Sem mensagem de erro)"}
+            </span>
+          </p>
+          <p>
+            Prioridade:{" "}
+            <span className="font-bold">
+              {formattedPriority(rule.priority)}
+            </span>
+          </p>
+        </div>
+      }
+    >
+      <Chip
+        label={handleLabel()}
+        onDelete={onDelete}
+        clickable
+        variant={theme === "dark" ? "outlined" : "filled"}
+        color={colors.muiColor}
+        sx={{
+          color: colors.color,
+          [theme === "dark" ? "borderColor" : "backgroundColor"]:
+            colors.backgroundColor,
+        }}
+      />
+    </Tooltip>
+  );
 };
 
 export default Validation;
