d526d1b5ec2731259ccd836b9ea435c0577495d5
Isadora Paz <mibp@ic.ufal.br>
2025-12-10 17:28:53 -0300

    :lipstick: improve toast styling and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { CheckCircle, Error, Info, Warning } from "@mui/icons-material";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import AuthProvider from "react-auth-kit";
@@ -6,10 +7,11 @@ import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { CompanyContextProvider } from "./contexts/companyContext";
-import { ThemeModeProvider } from "./contexts/themeModeContext";
+import { ThemeModeProvider, useThemeMode } from "./contexts/themeModeContext";
 import { UserStateProvider } from "./contexts/userState";
 import { AppRoutes } from "./routes/routes";
 import { qc } from "./services/queryClient";
+import { handleMode } from "./theme";
 
 const store = createStore({
   authType: "localstorage",
@@ -27,13 +29,32 @@ const Root = () => {
 };
 
 function App() {
+  const theme = handleMode(useThemeMode().mode);
   return (
     <QueryClientProvider client={qc}>
       <AuthProvider store={store}>
         <UserStateProvider>
           <CompanyContextProvider>
             <RouterProvider router={AppRoutes} />
-            <ToastContainer position="bottom-right" />
+            <ToastContainer
+              position="bottom-right"
+              pauseOnFocusLoss={false}
+              icon={({ type }) => {
+                switch (type) {
+                  case "success":
+                    return <CheckCircle color="success" />;
+                  case "error":
+                    return <Error color="error" />;
+                  case "warning":
+                    return <Warning color="warning" />;
+                  case "info":
+                    return <Info color="info" />;
+                  default:
+                    return null;
+                }
+              }}
+              theme={theme}
+            />
           </CompanyContextProvider>
         </UserStateProvider>
       </AuthProvider>
